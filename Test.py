022fe10596da2d1f0e14ee13015cd8c6e624e0a5
Mahdi <mahditaj2000@gmail.com>
2026-04-26 18:40:53 +0330

    I want some examples of ML csv(s) & some of my trys to do this with that csv files

--- Test.py
+++ Test.py
@@ -1,7 +1,48 @@
 
-ss = [("as",27) , ("ba",10) , ("cf" , 11) , ("daa" , 18)]
-ss.sort(reverse=True)
+import requests
+import json
 
-print(ss)
+# Define the inputBody and headers
+input_body = {
+    "tasks": {
+        "import": {
+            "operation": "import/upload"
+        },
+        "compress": {
+            "operation": "compress",
+            "input": "import",
+            "input_format": "mp3",
+            "output_format": "mp3",
+            "options": {
+                "compression_method": "percentage",
+                "target_size_percentage": 40
+            }
+        },
+        "export-url": {
+            "operation": "export/url",
+            "input": [
+                "compress"
+            ]
+        }
+    }
+} 
+
+headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Authorization': 'Bearer {access-token}'
+}
+
+url = "https://api.freeconvert.com/v1/process/jobs"
+
+# Make a POST request and print the response
+response = requests.post(url, data=json.dumps(input_body), headers=headers)
+
+if response.status_code == 200:
+    response_json = response.json()
+    print(response_json)
+else:
+    print(f"Request failed with status code: {response.status_code}")
+    print(response.text)
 
 
